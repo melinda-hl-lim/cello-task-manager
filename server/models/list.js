5846a02ea4d9dacfb54809dc8e57ecae3ae2e78e
Andrew Jones <andrewrayjones@gmail.com>
2021-03-22 12:46:07 -0500

    add List model

---
+++ server/models/list.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ListSchema = new Schema({
+  title: {
+    type: String,
+    required: [true, 'The List title is required']
+  },
+  boardId: { type: Schema.Types.ObjectId, ref: 'Board' },
+  position: Number,
+  cards: [{ type: Schema.Types.ObjectId, ref: 'Card' }]
+}, {timestamps: true})
+
+const List = mongoose.model('List', ListSchema);
+
+module.exports = List;
