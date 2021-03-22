616d913729c07f2c0de2baf72e5826e949628ccd
Andrew Jones <andrewrayjones@gmail.com>
2021-03-22 13:14:27 -0500

    getBoard method begun

--- server/controllers/boardsController.js
+++ server/controllers/boardsController.js
@@ -11,6 +11,19 @@ const getBoards = (req, res, next) => {
     })
 };
 
+const getBoard = (req, res, next) => {
+  Board.findById(req.params.id)
+    .populate("lists", { 
+      title: 1,
+      boardId: 1,
+      position: 1,
+      cards: // nested populate??
+     })
+    .then((board) => {
+
+    })
+}
+
 const createBoard = (req, res, next) => {
   const errors = validationResult(req);
   if (errors.isEmpty()) {
