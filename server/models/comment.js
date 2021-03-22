f46a07548b96a38f2797992f0aad3a8ca6cd6eb9
Andrew Jones <andrewrayjones@gmail.com>
2021-03-22 13:00:47 -0500

    add Comment model

---
+++ server/models/comment.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CommentSchema = new Schema({
+  cardId: { type: Schema.Types.ObjectId,
+            required: [true, 'The list id is required'],
+            ref: 'Card' 
+          },
+  text: {
+    type: String,
+    required: [true, 'The comment text is required']
+  }
+}, {timestamps: true})
+
+const Comment = mongoose.model('Comment', CommentSchema);
+
+module.exports = Comment;
