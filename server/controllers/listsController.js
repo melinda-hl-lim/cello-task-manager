b9dd80a483f7c29c30d85fd755019d9102973021
Andrew Jones <andrewrayjones@gmail.com>
2021-03-24 13:17:32 -0500

    server side Add List, with error for missing title

---
+++ server/controllers/listsController.js
@@ -0,0 +1,20 @@
+const List = require("../models/list");
+const HttpError = require("../models/httpError");
+const { validationResult } = require("express-validator");
+
+const createList = (req, res, next) => {
+  const errors = validationResult(req);
+  if (errors.isEmpty()) {
+    List.create(req.body)
+      .then((list) => {
+        List.findById(list.id).then(list => res.json({ list }))
+      })
+      .catch(err => 
+        next(new HttpError("Creating list failed, please try again", 500))
+      );
+  } else {
+    return next(new HttpError("The list title is empty.", 404));
+  }
+}
+
+exports.createList = createList;
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const boardsController = require("../controllers/boardsController");
+const listsController = require("../controllers/listsController");
 const { validateBoard } = require("../validators/validators");
+const { validateList } = require("../validators/validators");
 
 router.get('/boards/:id', boardsController.getBoard);
 router.get('/boards', boardsController.getBoards);
 
 router.post('/boards', validateBoard, boardsController.createBoard);
+
+router.post('/lists', validateList, listsController.createList);
 
 module.exports = router;
