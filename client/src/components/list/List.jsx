103cbb1f8aded45163110f7dc1d96a6e77538145
sjbabadi <sheila.babadi@yahoo.com>
2021-03-25 12:31:38 -0400

    conditionally render list title for editing

--- client/src/components/list/List.jsx
+++ client/src/components/list/List.jsx
@@ -1,24 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import CardTile from "./CardTile";
 
 const List = ({ id }) => {
-  const { position, title } = useSelector(state => state.lists.find(list => list.id === id))
-  const cards = useSelector(state => state.cards.filter(card => card.listId === id))
+  const { position, title } = useSelector((state) =>
+    state.lists.find((list) => list.id === id)
+  );
+  const cards = useSelector((state) =>
+    state.cards.filter((card) => card.listId === id)
+  );
+
+  const [editing, setEditing] = useState(false);
+  const [newTitle, setNewTitle] = useState(title);
 
   const cardComponents = cards
     ?.sort((a, b) => a.position - b.position)
-    .map(card =>
-      <CardTile key={card.id} id={card.id} />
-    )
-  
+    .map((card) => <CardTile key={card.id} id={card.id} />);
+
+  const handleTitleChange = (e) => {
+    setNewTitle(e.target.value);
+  };
+
+  const toggleEdit = (e) => {
+    e.preventDefault();
+    setEditing(!editing);
+  };
+
   return (
     <div className="list-wrapper">
       <div className="list-background">
         <div className="list">
           <a className="more-icon sm-icon" href=""></a>
           <div>
-            <p className="list-title">{title}</p>
+            {editing ? (
+              <input
+                className="list-title"
+                value={newTitle}
+                onChange={handleTitleChange}
+              />
+            ) : (
+              <p className="list-title" onClick={toggleEdit}>
+                {title}
+              </p>
+            )}
           </div>
           <div className="add-dropdown add-top">
             <div className="card"></div>
@@ -29,7 +53,7 @@ const List = ({ id }) => {
             </div>
           </div>
           <div id="cards-container" data-id="list-1-cards">
-            {cardComponents}  
+            {cardComponents}
           </div>
           <div className="add-dropdown add-bottom">
             <div className="card">
